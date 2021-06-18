a18684113f5c698534127747d9857ff88ebf8421
QYnote <jmhc6@naver.com>
2021-06-18 14:01:47 +0900

    0618 save
    1. 머신러닝 미션 save

---
+++ Python/0618 Mission01.py
@@ -0,0 +1,31 @@
+# SVM 알고리즘
+from sklearn import svm
+
+# 사용
+from sklearn.model_selection import train_test_split    # 데이터 분리용
+from sklearn import metrics
+import pandas as pd
+
+# 데이터 가져오기
+df = pd.read_df = pd.read_csv("winequality-white.csv")    # wine.csv 불러오기
+data = df.iloc[:, :-1]   # 2번째 열부터 data 지정
+label = df.iloc[:, [-1]] # 1번째 열을 정답 지정
+
+#훈련 : 테스트 = 3 : 7
+train_data, test_data, train_label, test_label  = train_test_split(data,label, test_size=0.3, shuffle=True)
+
+# 알고리즘 학습지 나열
+clf = svm.SVC(gamma='auto')
+
+# 학습
+clf.fit(train_data,train_label)
+
+# 정답률
+results = clf.predict(test_data)    # 모의고사 답안 제출
+score = metrics.accuracy_score(results, test_label)
+
+print("정달률 : %5.2f %%" % (score*100))
+
+# 정답률에 따른 save
+import joblib
+joblib.dump(clf, "ai_svm_JB_data" + str(int(score*100)) + ".dmp")
--- Python/MNIST Leraning.py
+++ Python/MNIST Leraning.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics, utils
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 # 0. 데이터 준비
 df = pd.read_csv("train_1k.csv")  # 28x28 크기의 RAW 이미지 1000건.
 # print(df.shape)
@@ -11,14 +12,18 @@ data = df.iloc[: , 1:] # 데이터(문제) 전체
 label = df.iloc[:, [0]] # 레이블(답) 전체
 train_data, test_data, train_label, test_label \
     = train_test_split(data, label, test_size=0.2, shuffle=True)
+
 # 1. 머신러닝 알고리즘 선택 (변경 후, 테스트...)
 clf = KNeighborsClassifier(n_neighbors=3)
+
 # 2. 학습시키기 ---> 오래 걸림
 clf.fit(train_data, train_label)  # clf가 '모델', '인공지능'
+
 # 3. 정답률 구하기 --> 몇점까리 모델이니?
 results = clf.predict(test_data)
 score = metrics.accuracy_score(results, test_label) # 채점하기
 print("정답률 : %5.2f %%" % (score*100))
+
 # 4. 결과 모델을 저장하기
 import joblib
 joblib.dump(clf, "ai_mnist_1k_knn_" + str(int(score*100)) + ".dmp")
